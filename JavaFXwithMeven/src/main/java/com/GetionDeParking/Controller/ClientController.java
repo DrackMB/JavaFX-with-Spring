839add64814f0fbbd84dcc0387a22fcaf955b701
DrackMB <58629183+DrackMB@users.noreply.github.com>
2020-04-03 03:00:05 +0100

    modifer date 03-04-2020 2:03 JavaFXv1

--- JavaFXwithMeven/src/main/java/com/GetionDeParking/Controller/ClientController.java
+++ JavaFXwithMeven/src/main/java/com/GetionDeParking/Controller/ClientController.java
@@ -5,10 +5,46 @@
  */
 package com.GetionDeParking.Controller;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import retrofit2.GsonConverterFactory;
+import retrofit2.Retrofit;
+
 /**
  *
  * @author dell
  */
-public class ClientController {
+public class ClientController implements Initializable  {
+     static final String BASE_URL = "http://localhost:8090/";
+    Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+         Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+    
+    @FXML
+    private Label label;
+    @FXML
+    private Button reserver;
+    
+    
+    @FXML
+    private void AccetClient(ActionEvent event){
+        
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
     
 }
--- JavaFXwithMeven/src/main/java/com/compati/test22/RequetAdmine.java
+++ JavaFXwithMeven/src/main/java/com/compati/test22/RequetAdmine.java
@@ -6,8 +6,11 @@
 package com.compati.test22;
 
 import com.GestionDeParking.bean.Administrateur;
+import com.GestionDeParking.bean.Reservation;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 /**
@@ -18,5 +21,11 @@ public interface RequetAdmine {
 
     @GET("GestionDeParking/Admin/user/{users}")
     public Call<Administrateur> findByLogin(@Path("users") String user);
+
+    @POST("/")
+    public Call<Reservation> save(@Body Reservation reservation);
+    
+    
+    
 
 }
